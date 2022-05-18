8546afa2764a382c8982cb5b7f555af73dcc6e91
mohamed-shaban2001 <100471890+mohamed-shaban2001@users.noreply.github.com>
2022-05-18 02:48:36 +0200

    Add files via upload

---
+++ onl/controllers/account-user-data.php
@@ -0,0 +1,37 @@
+
+<?php
+if(strlen($_SESSION['login'])==0)
+    {   
+header('location:.php');
+}
+else{
+	// code for billing address updation
+	if(isset($_POST['update']))
+	{
+		$baddress=$_POST['billingaddress'];
+		$bstate=$_POST['bilingstate'];
+		$bcity=$_POST['billingcity'];
+		$bpincode=$_POST['billingpincode'];
+		$query=mysqli_query($con,"update users set billingAddress='$baddress',billingState='$bstate',billingCity='$bcity',billingPincode='$bpincode' where id='".$_SESSION['id']."'");
+		if($query)
+		{
+echo "<script>alert('Billing Address has been updated');</script>";
+		}
+	}
+
+
+// code for Shipping address updation
+	if(isset($_POST['shipupdate']))
+	{
+		$saddress=$_POST['shippingaddress'];
+		$sstate=$_POST['shippingstate'];
+		$scity=$_POST['shippingcity'];
+		$spincode=$_POST['shippingpincode'];
+		$query=mysqli_query($con,"update users set shippingAddress='$saddress',shippingState='$sstate',shippingCity='$scity',shippingPincode='$spincode' where id='".$_SESSION['id']."'");
+		if($query)
+		{
+echo "<script>alert('Shipping Address has been updated');</script>";
+		}
+	}
+
+?>
---
+++ onl/controllers/user-check-connection.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_GET['action']) && $_GET['action']=="add"){
+	$id=intval($_GET['id']);
+	if(isset($_SESSION['cart'][$id])){
+		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql_p="SELECT * FROM products WHERE id={$id}";
+		$query_p=mysqli_query($con,$sql_p);
+		if(mysqli_num_rows($query_p)!=0){
+			$row_p=mysqli_fetch_array($query_p);
+			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);
+		
+		}else{
+			$message="Product ID is invalid";
+		}
+	}
+		echo "<script>alert('Product has been added to the cart')</script>";
+		echo "<script type='text/javascript'> document.location ='my-cart.php'; </script>";
+}
+?>
